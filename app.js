64e46e00e070a7200249f07e92a0dad164737098
dd-Script <softengdavid.dev@gmail.com>
2025-07-01 15:30:51 +0100

    commit

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const connectDB = require('./src/config/db');
 const studentRoutes = require('./src/routes/student.routes');
 
 require('dotenv').config();
-// create an Express application
+
 const app = express();
 
-// middleware: parse incoming JSON bodies into req.body
 app.use(express.json());
 
-// ROUTES
+app.get('/', (req, res)=> {
+    res.send('Welcome To My Home Page!');
+})
+
 app.use('/students', studentRoutes);
 
-// === GLOBAL ERROR HANDLER ===
+// GLOBAL ERROR HANDLER
 // Catches any thrown error and sends a JSON response
 app.use((err, req, res, next) => {
   console.error('Error handler caught:', err);
@@ -22,7 +24,7 @@ app.use((err, req, res, next) => {
     .json({ error: err.message || 'Internal Server Error' });
 });
 
-// Start Server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   connectDB()
